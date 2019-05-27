cd80996a6c04e268533b88ddc7dcacce17bff5a7
dayan17 <172137321@qq.com>
2019-05-27 14:18:30 +0800

    home页面的修改

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,9 +1,19 @@
 const proxy = require('http-proxy-middleware');
 
 module.exports = function(app) {
-  app.use(proxy('/ajax', { 
-      target: 'http://m.maoyan.com',
-      changeOrigin:true  
-  }));
+  app.use(proxy('/rms', { 
+    target: 'https://dsapi.beibei.com',
+    changeOrigin:true  
+}));
+
+app.use(proxy('/ads', { 
+  target: 'https://dsapi.beibei.com',
+  changeOrigin:true  
+}));
+
+app.use(proxy('/martshow', { 
+  target: 'https://dsapi.beibei.com',
+  changeOrigin:true  
+}));
 
 };
